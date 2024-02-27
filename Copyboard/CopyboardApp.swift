c7c45f640f5ad86db2c5f9c571241747d5a9c960
TopScrech <sergei_saliukov@icloud.com>
2024-02-27 10:31:36 +0100

    Initial Commit

---
+++ Copyboard/CopyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyboardApp.swift
+//  Copyboard
+//
+//  Created by Sergei Saliukov on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CopyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
